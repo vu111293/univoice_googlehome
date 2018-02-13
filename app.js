22539550dfbe57ea77d628ac6bb580ada74e2b54
Lê Quốc Vũ <vu111293@gmail.com>
2018-02-13 14:12:52 +0700

    update voice sample

--- app.js
+++ app.js
@@ -9,20 +9,44 @@ let bodyParse = require('body-parser');
 let sprintf = require('sprintf-js').sprintf;
 
 
-const FINDBY_TIME_PROMPT = ["Không hiểu. Để tìm theo thời gian, bạn có thể nói \"tìm univoice files vào ngày xxx\"."
-    + " Để tìm theo địa điểm, bạn có thể nói \"tìm univoice files tại xxx\""];
+// const FINDBY_TIME_PROMPT = ["Không hiểu. Để tìm theo thời gian, bạn có thể nói \"tìm univoice files vào ngày xxx\"."
+//     + " Để tìm theo địa điểm, bạn có thể nói \"tìm univoice files tại xxx\""];
+
+const SAMPLE_NOTES = [
+    "In 21/10/2017, find and book train for Tet festival",
+    "On 10:30 morning, metting in my company",
+    "Evening, go to coffeshop and make payment",
+    "20/10/2017, metting old friend and party",
+    "On 14:30, 30/10/2017, check healty at Adan Hopital",
+    "Join to football club at Phu Tho stadium",
+    "No found"
+];
+
+const FINDBY_TIME_PROMPT = ["I don't understand. To find by time, you can say \"Find univoice files plus day.\""
+    + " To find by place, you can say \"Find univoice files plus place name\"", "Say \"Find univoice files plus anything you think\""];
+
+const WELCOME_PROMPT = [
+    "Hey. I'm Univoice. Can i help u?",
+    "Hi. I can find you note. Please ask me",
+    "Hello. Nice to meet u",
+    "Ok. Ask me to your note"
+];
 
 
 let app = express();
 app.set('port', (process.env.PORT || 8080));
 app.use(bodyParse.json({ type: 'application/json' }));
 
+const WELCOME_ACTION = 'welcome';
 const FINDBY_PLACE_ACTION = 'find_by_place';
 const FINDBY_TIME_ACTION = 'find_by_time';
 
 let actionMap = new Map();
+
+actionMap.set(WELCOME_ACTION, welcome);
 actionMap.set(FINDBY_PLACE_ACTION, findByPlace);
 actionMap.set(FINDBY_TIME_ACTION, findByTime);
+
 
 app.post('/', function (request, response) {
     console.log('header: ' + JSON.stringify(request.headers));
@@ -40,13 +64,19 @@ var server = app.listen(app.get('port'), function () {
     console.log('Press Ctrl+C to quit.');
 });
 
+
+
 // handler api
+function welcome(app) {
+    app.ask(getRandomPrompt(app, WELCOME_PROMPT));
+}
+
 function findByPlace(app) {
     let place = app.getArgument('place');
     if (place) {
-        app.tell("Bạn đã yêu cầu tìm ghi chú tại " + place.toUpperCase());
+        app.tell("Find note at " + place.toUpperCase() + ". " + getRandomPrompt(app, SAMPLE_NOTES));
     } else {
-        app.ask("Bạn muốn tìm univoice note ở đâu?");
+        app.ask("Where do you want to find the note?");
     }
 }
 
@@ -59,32 +89,32 @@ function findByTime(app) {
 
     if (day || month || year) {
         var fullDate = "";
-        if (day) { 
-            fullDate += " ngày " + day; }
+        if (day) {
+            fullDate += "/" + day;
+        }
         if (month) {
-            fullDate += " tháng " + month;
+            fullDate += "/" + month;
         }
         if (year) {
-            fullDate += " năm " + year;
+            fullDate += "/" + year;
         }
 
         if (aboutTime) {
-            app.tell("Bạn đã yêu cầu tìm univoice note vào " + aboutTime.toUpperCase() + fullDate.toLocaleUpperCase());
+            app.tell("Find note at " + aboutTime.toUpperCase() + fullDate.toLocaleUpperCase() + ". " + getRandomPrompt(app, SAMPLE_NOTES));
         } else {
-            app.tell("Bạn đã yêu cầu tìm univoice note vào" + fullDate.toLocaleUpperCase());
+            app.tell("Find note at " + fullDate.toLocaleUpperCase()+ ". " + getRandomPrompt(app, SAMPLE_NOTES));
         }
     } else {
         // case tuần trước có univoice gì?
         // case có univoice gì vào tuần trước?
 
         if (dynamicTime) {
-            app.tell("Bạn đã yêu cầu tìm univoice note vào " + dynamicTime.toUpperCase());
+            app.tell("Find note at " + dynamicTime.toUpperCase() + ". " + getRandomPrompt(app, SAMPLE_NOTES));
         } else {
             app.ask(getRandomPrompt(app, FINDBY_TIME_PROMPT));
         }
     }
 }
-
 
 // Utility function to pick prompts
 function getRandomPrompt(app, array) {
